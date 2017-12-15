e84a4d7aaa6fb33ab6f75bb0eb0aba56204f51ac
4N4RCHY <anon.dark97@gmail.com>
2017-12-15 19:35:50 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,25 @@
 <!DOCTYPE html>
 <!-- Snake Game v2 -->
-<!-- index.html -->
+<!-- index.php -->
 <html>
+	<?php
+		$host = ''; // hôte de la base de donnée
+		$dbName = ''; // nom de la base de donnée
+		$encoding = ''; // encodage à utiliser
+		$username = ''; // utilisateur de connexion
+		$password = ''; // mot de passe utilisateur
+		
+		try { $bdd = new PDO("mysql:host=$host;dbname=$dbName;charset=$encoding", $username, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING)); }
+		catch(Exception $e) { die('ERROR: '.$e->getMessage()); }
+		
+		$data = $bdd->query("SELECT * FROM snake ORDER BY score DESC");
+		$player = array(); $score = array();
+		for($i = 0; $i < 10; $i++) {
+			$output = $data->fetch(PDO::FETCH_NUM);
+			$player[$i] = $output[0];
+			$score[$i] = $output[1];
+		}
+	?>
 	<head>
 		<meta charset="UTF-8">
 		<title>Snake Game v2</title>
@@ -16,8 +34,8 @@
 	<script>
 		document.getElementById('sound').volume = 0.70;
 		var leaderboard = {
-			'player': ['', '', '', '', '', '', '', '', '', ''],
-			'score': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+			'player': [<?php echo("'$player[0]', '$player[1]', '$player[2]', '$player[3]', '$player[4]', '$player[5]', '$player[6]', '$player[7]', '$player[8]', '$player[9]'"); ?>],
+			'score': [<?php echo("$score[0], $score[1], $score[2], $score[3], $score[4], $score[5], $score[6], $score[7], $score[8], $score[9]"); ?>]
 		};
 	</script>
 </html>
