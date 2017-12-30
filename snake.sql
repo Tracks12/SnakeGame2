ee7998d863e22c85dc5c4c7abef1ccd0d2bbd461
4N4RCHY <anon.dark97@gmail.com>
2017-12-30 01:48:37 +0100

    Update snake.sql

--- snake.sql
+++ snake.sql
@@ -5,7 +5,7 @@ CREATE DATABASE IF NOT EXISTS `snake`;
 
 CREATE TABLE IF NOT EXISTS `snake` (
 	`player` varchar(255),
-  `score` int(255) NOT NULL DEFAULT 0
+	`score` int(255) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- END
